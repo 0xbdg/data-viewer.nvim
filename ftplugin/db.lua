d852b4ad4f453ddb513fe8d767f434a7d6d54556
?0 <benjamindguntara@protonmail.com>
2025-09-06 15:18:18 +0000

    djasidaj

--- ftplugin/db.lua
+++
@@ -1,9 +0,0 @@
-if vim.b.did_data_viewer_ftplugin == 1 then
-  return
-end
-
-local config = require("data-viewer.config")
-
-if not config.config.skipSqlite then
-  vim.schedule(function () require("data-viewer").start({ args = "", force_replace = true }) end)
-end
